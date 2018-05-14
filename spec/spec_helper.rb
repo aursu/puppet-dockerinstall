69f8b4ad3dcac600733a3b490f04c5c912d5970d
Alexander Ursu <alexander.ursu@gmail.com>
2018-05-14 04:16:01 +0200

    Passes pdk test unit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,18 @@ end
 
 include RspecPuppetFacts
 
+# TODO: http://unethicalblogger.com/2014/03/01/testing-custom-facts-with-rspec.html
+
+# https://github.com/mcanevet/rspec-puppet-facts/blob/master/README.md#create-dynamic-facts
+# register is_init_systemd fact from module lsys
+add_custom_fact :is_init_systemd, ->(os, _facts) do
+  if os == 'ubuntu-14.04-x86_64'
+    false
+  else
+    true
+  end
+end
+
 default_facts = {
   puppetversion: Puppet.version,
   facterversion: Facter.version
