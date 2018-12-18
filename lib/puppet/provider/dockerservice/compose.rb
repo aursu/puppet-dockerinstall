ca8fd31fc425fe5d41f769cef6c3caa1fa7df945
Alexander Ursu <alexander.ursu@gmail.com>
2018-12-18 12:39:51 +0100

    Completed dockerservice type - ready for testing

--- lib/puppet/provider/dockerservice/compose.rb
+++ lib/puppet/provider/dockerservice/compose.rb
@@ -1,28 +1,27 @@
 Puppet::Type.type(:glance_api_config).provide(
-      :ini_setting,
-      # set ini_setting as the parent provider
-      :parent => Puppet::Type.type(:service).provider(:base)
-    ) do
-      @doc = 'Docker service provider'
+  :ini_setting,
+  # set ini_setting as the parent provider
+  :parent => Puppet::Type.type(:service).provider(:base), # rubocop:disable Style/HashSyntax
+) do
+  @doc = 'Docker service provider'
 
-      def self.basedir
-        if File.directory?('/run')
-          '/run/compose'
-        else
-          '/var/run/compose'
-        end
-      end
+  def self.basedir
+    if File.directory?('/run')
+      '/run/compose'
+    else
+      '/var/run/compose'
+    end
+  end
 
-      commands compose: 'docker-compose'
+  commands compose: 'docker-compose'
 
-      if command('compose')
-        confine true: begin
-                        compose('--version')
-                      rescue Puppet::ExecutionFailure
-                        false
-                      else
-                        true
-                      end
-      end
+  if command('compose')
+    confine true: begin
+                    compose('--version')
+                  rescue Puppet::ExecutionFailure
+                    false
+                  else
+                    true
+                  end
+  end
 end
-
