cc619243fea161d58c099434fc659dfa8641eadd
Alexander Ursu <alexander.ursu@gmail.com>
2019-03-14 12:24:07 +0100

    Added never versions into Version type

--- types/version.pp
+++ types/version.pp
@@ -1,5 +1,5 @@
 type Dockerinstall::Version = Variant[
     Enum['present', 'installed', 'absent'],
-    Pattern[/^17\.1[0-2]\./, /^17\.0[3-9]\./, /^18\.01\./],  # https://docs.docker.com/release-notes/docker-ce/
-    Pattern[/^1\.12\.6/, /^1\.13\.[01]/]                     # https://docs.docker.com/release-notes/docker-engine/
+    Pattern[/^17\.1[0-2]\./, /^17\.0[3-9]\./, /^18\.0[1-9]\./],  # https://docs.docker.com/engine/release-notes/
+    Pattern[/^1\.12\.6/, /^1\.13\.[01]/]                         # https://docs.docker.com/release-notes/docker-engine/
 ]
