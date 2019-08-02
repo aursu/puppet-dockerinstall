a6c38ee926c593f2afdb8500c2715ea91c0c372d
Alexander Ursu <alexander.ursu@gmail.com>
2019-08-02 13:30:30 +0200

    Added debugging info

--- lib/facter/docker_info.rb
+++ lib/facter/docker_info.rb
@@ -22,10 +22,13 @@ Facter.add(:docker_swarm) do
   setcode do
     info = Facter.value(:docker_info)
 
+    warning _("docker_info: #{info}")
     if info.empty?
       {}
     else
       swarm = info['Swarm']
+
+      warning _("swarm: #{swarm}")
 
       # it could be nil on this point when Docker daemon is not running
       return {} if swarm.nil?
