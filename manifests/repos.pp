fc841dae0d63d741776bfe8fd884d0853c577713
Alexander Ursu <alexander.ursu@gmail.com>
2019-12-03 23:55:09 +0100

    Added support for Ubuntu

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -11,19 +11,49 @@ class dockerinstall::repos (
     Boolean $gpgcheck               = $dockerinstall::repo_gpgcheck,
     String  $basearch               = $::architecture,
     String  $releasever             = $::operatingsystemmajrelease,
+    Boolean $repo_mgmt_software     = $dockerinstall::repo_management_software,
 )
 {
-    # https://docs.docker.com/install/linux/docker-ce/fedora/#set-up-the-repository
-    # https://docs.docker.com/install/linux/docker-ce/centos/#set-up-the-repository
-    $baseurl = "${location}/${os}/${releasever}/${basearch}/${repo}"
-    $gpgkey = "${location}/${os}/gpg"
+  # https://docs.docker.com/install/linux/docker-ce/fedora/#set-up-the-repository
+  # https://docs.docker.com/install/linux/docker-ce/centos/#set-up-the-repository
+  $distrourl = "${location}/${os}"
+  $rpmurl = "${distrourl}/${releasever}/${basearch}/${repo}"
+  $gpgkey = "${distrourl}/gpg"
 
-    if $manage_package {
-        yumrepo { 'docker':
-            descr    => 'Docker',
-            baseurl  => $baseurl,
-            gpgkey   => $gpgkey,
-            gpgcheck => $gpgcheck,
+  if $manage_package {
+    if $::operatingsystem == 'Ubuntu' {
+      if $repo_mgmt_software {
+        package {
+          [
+            'apt-transport-https',
+            'software-properties-common',
+          ]:
+          ensure => installed,
         }
+      }
+
+      # https://docs.docker.com/install/linux/docker-ce/ubuntu/
+      $aptrepo = "deb [arch=${basearch}] ${distrourl} ${::lsbdistcodename} ${repo}"
+      exec { 'aptrepo-docker':
+        command => "add-apt-repository \"${aptrepo}\"",
+        unless  => "grep ${distrourl} /etc/apt/sources.list",
+        path    => '/bin:/usr/bin',
+      }
+      if $gpgcheck {
+        exec { 'aptrepo-docker-gpgkey':
+          command => "curl -fsSL ${gpgkey} | apt-key add -",
+          unless  => 'apt-key fingerprint 0EBFCD88 | grep Docker',
+          path    => '/bin:/usr/bin',
+        }
+      }
     }
+    else {
+      yumrepo { 'docker':
+        descr    => 'Docker',
+        baseurl  => $rpmurl,
+        gpgkey   => $gpgkey,
+        gpgcheck => $gpgcheck,
+      }
+    }
+  }
 }
