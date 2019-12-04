a793c063f318cf7d5f84260c87eb70fa5702f085
Alexander Ursu <alexander.ursu@gmail.com>
2019-12-04 02:13:55 +0100

    Try to fix text table interruption for docker-compose output

--- lib/puppet/provider/dockerservice/compose.rb
+++ lib/puppet/provider/dockerservice/compose.rb
@@ -31,7 +31,8 @@ Puppet::Type.type(:dockerservice).provide(
   end
 
   def texecute(type, command, fof = true, squelch = false, combine = true)
-    execute(command, failonfail: fof, override_locale: false, squelch: squelch, combine: combine)
+    execute(command,  failonfail: fof, override_locale: false, squelch: squelch, combine: combine,
+                      custom_environment: { 'COLUMNS' => 512 })
   rescue Puppet::ExecutionFailure => detail
     @resource.fail Puppet::Error, "Could not #{type} #{@resource.ref}: #{detail}", detail
   end
