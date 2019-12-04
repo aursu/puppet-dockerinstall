54362e6f02d76f1166b8eeeb2e4bd99d515b5dab
Alexander Ursu <alexander.ursu@gmail.com>
2019-12-04 02:46:05 +0100

    Added epoch version for Ubuntu 16.04

--- types/version.pp
+++ types/version.pp
@@ -1,5 +1,5 @@
 type Dockerinstall::Version = Variant[
     Enum['present', 'installed', 'absent'],
-    Pattern[/^17\.1[0-2]\./, /^17\.0[3-9]\./, /^18\.0[1-9]\./, /^19\.03\./],  # https://docs.docker.com/engine/release-notes/
+    Pattern[/^17\.1[0-2]\./, /^17\.0[3-9]\./, /^18\.0[1-9]\./, /^(5:)?19\.03\./],  # https://docs.docker.com/engine/release-notes/
     Pattern[/^1\.12\.6/, /^1\.13\.[01]/]                         # https://docs.docker.com/release-notes/docker-engine/
 ]
