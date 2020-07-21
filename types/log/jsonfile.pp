aecf6e90bab6255edf8d96cec326729d31671115
Alexander Ursu <alexander.ursu@gmail.com>
2020-07-21 21:57:51 +0200

    Added type Dockerinstall::Log::JSONFile

---
+++ types/log/jsonfile.pp
@@ -0,0 +1,9 @@
+# https://docs.docker.com/config/containers/logging/json-file/
+type Dockerinstall::Log::JSONFile = Struct[{
+  Optional['max-size'] => Variant[Pattern[/^[0-9]+[gmk]?$/], Enum['-1']],
+  Optional['max-file'] => Variant[Pattern[/^[0-9]+$/]],
+  Optional['labels'] => String,
+  Optional['env'] => String,
+  Optional['env-regex'] => String,
+  Optional['compress'] => Enum['true', 'false'],
+}]
