3beec0cd174847fb88de885e96442848f8cc341f
Alexander Ursu <alexander.ursu@gmail.com>
2023-01-13 17:54:57 +0100

    Added few unit tests to cover default docker directories setup

--- spec/classes/setup_spec.rb
+++ spec/classes/setup_spec.rb
@@ -10,6 +10,48 @@ describe 'dockerinstall::setup' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      it {
+        is_expected.to contain_file('/etc/docker')
+          .with_ensure('directory')
+      }
+
+      it {
+        is_expected.to contain_file('/etc/docker/certs.d')
+          .with_ensure('directory')
+          .with_mode('0700')
+      }
+
+      it {
+        is_expected.to contain_file('/etc/docker/tls')
+          .with_ensure('directory')
+          .with_mode('0700')
+      }
+
+      context 'with TLS access to other system users' do
+        let(:params) do
+          {
+            users_access: true,
+          }
+        end
+        it {
+          is_expected.to contain_file('/etc/docker/tls')
+            .with_ensure('directory')
+            .with_mode('0711')
+        }
+      end
+
+      context 'with docker directory removal' do
+        let(:params) do
+          {
+            docker_dir_ensure: 'absent',
+          }
+        end
+        it {
+          is_expected.to contain_file('/etc/docker')
+            .with_ensure('absent')
+        }
+      end
     end
   end
 end
