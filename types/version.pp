2e545d81f08052d90fad3662dbf9e2d69b694d16
Alexander Ursu <alexanderu@crytek.de>
2024-12-18 20:38:50 +0100

    Added 27.* into list

--- types/version.pp
+++ types/version.pp
@@ -2,11 +2,6 @@ type Dockerinstall::Version = Variant[
   Enum['present', 'installed', 'absent'],
   # https://docs.docker.com/engine/release-notes/
   Pattern[
-    /^17\.12\./,
-    /^17\.0[369]\./,
-    /^18\.0[369]\./,
-    /^(3:)?19\.03\./,
-    /^([35]:)?20\.10\./,
-    /^([135]:)?2[3-6]\.[01]\./,
+    /^([135]:)?2[3-7]\.[0-4]\./,
   ],
 ]
